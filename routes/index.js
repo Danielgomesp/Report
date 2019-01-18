4ac8394db13fe9ab206bf6128d0a311e397b4653
danielgomesp <danielgomesp@gmail.com>
2019-01-18 15:27:52 -0200

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Umbler' });
+  res.render('index', { title: 'Umbler'});
 });
 
 module.exports = router;
